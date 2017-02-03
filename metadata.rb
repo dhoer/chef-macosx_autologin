c08afd6e89d0996d92ea5a30caa281258884bc99
Dennis Hoer <dennis.hoer@pearson.com>
2017-02-03 15:43:44 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description "Configures automatic login using a Gavin Brock's kcpassword"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.0.0'
+version '4.0.0'
 
 supports 'mac_os_x', '>= 10.7'
 
